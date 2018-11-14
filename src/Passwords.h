61c0f87a6ec2ef9962d60efba6a85b9f1644c51b
NitramLegov <Martin_Vogel@live.com>
2018-11-14 07:49:38 +0100

    Create Passwords.h

---
+++ src/Passwords.h
@@ -0,0 +1,7 @@
+// Demo file for "Passwords.h"
+
+const char* ssid = "YOURWIFISSDI";
+const char* password = "YOURWIFIPASSWORD";
+const char* OTA_password = "";
+int port = 1883;
+char* hostname = "YOURMQTTBROKER";
