7a413a1de4ec8435e7678b919a26c15ee29fabfc
NitramLegov <Martin.Vogel@live.com>
2018-11-17 22:52:18 +0100

    documentation

---
+++ src/Passwords.h
@@ -0,0 +1,6 @@
+// Demo file for "Passwords.h"
+ const char* ssid = "YOURWIFISSDI";
+const char* password = "YOURWIFIPASSWORD";
+const char* OTA_password = "";
+int port = 1883;
+char* hostname = "YOURMQTTBROKER";
